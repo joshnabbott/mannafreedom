bde7df262637b99dee1529b8c4861a108fe0e62e
Josh N. Abbott <joshnabbott@gmail.com>
2011-05-15 13:13:27 -0600

    Added the full footer.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
   // Slideshow
-  setTimeout(function(){
-    $('#slideshow').nivoSlider({
-      effect:                   'fade',
-      directionNav:             true,
-      directionNavHide:         false,
-      pauseTime:                5000,
-      pauseOnHover:             true,
-      controlNavThumbs:         false,
-      slices:                   1
-    });
-  }, 2000);
+  $('#slideshow').nivoSlider({
+    effect:           'fade',
+    directionNav:     true,
+    directionNavHide: false,
+    pauseTime:        5000,
+    pauseOnHover:     true,
+    controlNavThumbs: false,
+    slices:           1
+  });
 })
